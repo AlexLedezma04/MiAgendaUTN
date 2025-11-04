6345c20c4141fc0684d6aa7cec2143db247aae22
DaniSaborio <danisabo8713@gmail.com>
2025-11-04 00:58:41 -0600

    Actualizacion de frontend

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
+//
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
